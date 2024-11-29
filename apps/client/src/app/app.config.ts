d8b2d8a686e75800fcf0f7a418fe686e41bacd0d
Arnd Westermann <arnd.westermann@precess.de>
2024-11-29 13:01:29 +0100

    feat(general): add multi lang support

--- apps/client/src/app/app.config.ts
+++ apps/client/src/app/app.config.ts
@@ -1,7 +1,34 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode, APP_INITIALIZER } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco, TranslocoService } from '@jsverse/transloco';
+
+export function initializeApplication(translocoService: TranslocoService): () => void {
+	return () => translocoService.setActiveLang(navigator.language.split('-')[0]);
+}
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes)],
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(appRoutes),
+		provideHttpClient(),
+		provideTransloco({
+			config: {
+				availableLangs: ['en', 'de'],
+				defaultLang: 'en',
+				// Remove this option if your application doesn't support changing language in runtime.
+				reRenderOnLangChange: true,
+				prodMode: !isDevMode(),
+			},
+			loader: TranslocoHttpLoader,
+		}),
+		{
+			provide: APP_INITIALIZER,
+			useFactory: initializeApplication,
+			deps: [TranslocoService],
+			multi: true,
+		},
+	],
 };

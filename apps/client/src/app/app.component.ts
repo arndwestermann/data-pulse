41d6ed6b0d9d95cbfc6af5ab5a97aabfa505ca23
Arnd Westermann <arnd.westermann@precess.de>
2025-01-04 18:50:33 +0100

    feat(client): add global loading spinner

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TuiThemeColorService } from '@taiga-ui/cdk';
-import { TuiRoot } from '@taiga-ui/core';
-import { UserService } from './shared/services';
+import { TuiLoader, TuiRoot } from '@taiga-ui/core';
+import { AppService, UserService } from './shared/services';
 import { take } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
-	imports: [RouterModule, TuiRoot],
+	imports: [RouterModule, TuiRoot, TuiLoader],
 	selector: 'dp-root',
 	template: `
-		<tui-root class="h-full" tuiTheme="light">
-			<router-outlet />
+		<tui-root class="h-full bg-red-500" tuiTheme="light">
+			<tui-loader [overlay]="true" [showLoader]="isLoading()" class="h-full">
+				<router-outlet />
+			</tui-loader>
 		</tui-root>
 	`,
 	styles: `
@@ -22,6 +25,9 @@ import { take } from 'rxjs';
 export class AppComponent implements OnInit {
 	private readonly theme = inject(TuiThemeColorService);
 	private readonly userService = inject(UserService);
+	private readonly appService = inject(AppService);
+
+	public readonly isLoading = toSignal(this.appService.isLoading$, { initialValue: false });
 
 	public ngOnInit(): void {
 		this.theme.color = '#393D47';

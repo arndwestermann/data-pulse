6660ef42b6a92b8d1f50d26b04bfcc66de24f52e
Arnd Westermann <arnd.westermann@precess.de>
2025-01-06 16:50:20 +0100

    change(api): fix startup script

--- apps/api/src/startup.sh
+++ apps/api/src/startup.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-cd ../data-pulse && npx -c "PRODUCTION=true typeorm -d ./dist/apps/api/database/database.config.js migration:run" && cd ../current
-node "./main.js"
+npx -c "PRODUCTION=true typeorm -d ./database/database.config.js migration:run" && node "./main.js"

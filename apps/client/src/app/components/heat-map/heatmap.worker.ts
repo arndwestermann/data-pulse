330209387dda43cb228418afc34c95490dec144e
Arnd Westermann <arnd.westermann@precess.de>
2025-02-27 16:15:46 +0100

    feat(heatmap): offload creating heatmap to webworkers

---
+++ apps/client/src/app/components/heat-map/heatmap.worker.ts
@@ -0,0 +1,14 @@
+import { DoWork, runWorker } from 'observable-webworker';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { getHeatMap } from './utils/get-heat-map/get-heat-map.util';
+import { IRecord } from '../../shared/models';
+import { IHeatMap } from './models/heat-map.model';
+
+export class HeatmapWorker implements DoWork<{ records: IRecord[]; from: Date; to: Date }, IHeatMap[]> {
+	public work(input$: Observable<{ records: IRecord[]; from: Date; to: Date }>): Observable<IHeatMap[]> {
+		return input$.pipe(map(({ records, from, to }) => getHeatMap(records, from, to)));
+	}
+}
+
+runWorker(HeatmapWorker);

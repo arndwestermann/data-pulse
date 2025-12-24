13b968447bfb40c10d69b3cfd81bc14924f0052f
Arnd Westermann <arnd.westermann@precess.de>
2025-12-24 11:35:13 +0100

    chore(general): update NestJS to v11.x

--- apps/api/src/app/authentication/strategy/jwt.strategy.ts
+++ apps/api/src/app/authentication/strategy/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: false,
-			secretOrKey: configService.get('JWT_ACCESS_TOKEN_SECRET'),
+			secretOrKey: configService.getOrThrow('JWT_ACCESS_TOKEN_SECRET'),
 		});
 	}
 

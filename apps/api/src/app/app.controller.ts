4722495f2aac92476d109084e55e48ca9a73ecc4
Arnd Westermann <westermann.arnd@web.de>
2026-05-02 16:22:37 +0200

    change(api): remove `AppService`

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,20 +1,12 @@
 import { Controller, Get, Res } from '@nestjs/common';
-import { AppService } from './app.service';
 import { Response } from 'express';
 import { Public } from './authentication/decorators';
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
-
-	@Get()
-	helloWord() {
-		return this.appService.helloWorld();
-	}
-
 	@Get('health')
 	@Public()
-	health(@Res() res: Response) {
-		res.status(418).send({ ping: 'Ok!' });
+	public health(@Res() res: Response): void {
+		res.status(418).send({ ping: 'Ok' });
 	}
 }
